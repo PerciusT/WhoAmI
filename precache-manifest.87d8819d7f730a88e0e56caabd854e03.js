1f2ed45fe0e8a0c30b3e677a646043a487c3e029
PerciusT <shari200089@gmail.com>
2023-05-29 17:18:36 +0500

    Updates

---
+++ precache-manifest.87d8819d7f730a88e0e56caabd854e03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "16534f0402c542c2da2a",
+    "url": "/WhoAmI/static/css/main.2a15a7c6.chunk.css"
+  },
+  {
+    "revision": "16534f0402c542c2da2a",
+    "url": "/WhoAmI/static/js/main.16534f04.chunk.js"
+  },
+  {
+    "revision": "dfe2139a77f4f346d04e",
+    "url": "/WhoAmI/static/css/1.8990520b.chunk.css"
+  },
+  {
+    "revision": "dfe2139a77f4f346d04e",
+    "url": "/WhoAmI/static/js/1.dfe2139a.chunk.js"
+  },
+  {
+    "revision": "dba041dafc5a2cd0ee42",
+    "url": "/WhoAmI/static/js/2.dba041da.chunk.js"
+  },
+  {
+    "revision": "3a4ab2e6cd592168c352",
+    "url": "/WhoAmI/static/js/3.3a4ab2e6.chunk.js"
+  },
+  {
+    "revision": "13ebb7f32c268f481c76",
+    "url": "/WhoAmI/static/js/runtime~main.13ebb7f3.js"
+  },
+  {
+    "revision": "0c5482be7d45bb8d1e6f15ed40be7afb",
+    "url": "/WhoAmI/index.html"
+  }
+];
